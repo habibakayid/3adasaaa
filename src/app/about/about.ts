8bf9bac2401ea1da800be80b822a9c1803745472
habibakayid <habibakayid@gmail.com>
2026-09-20 15:25:28 +0300

    **

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,21 +1,22 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import postsData from '../../assets/posts.json';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
 export class About {
+  siteInfo = postsData.siteInfo;
   authors = this.getUniqueAuthors();
 
   getUniqueAuthors() {
-  return postsData.posts
-    .map(post => post.author)
-    .filter((author, index, arr) => 
-      arr.findIndex(a => a.name === author.name) === index
-    );
-}
-
+    return postsData.posts
+      .map(post => post.author)
+      .filter((author, index, arr) =>
+        arr.findIndex(a => a.name === author.name) === index
+      );
+  }
 }
